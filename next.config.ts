87ae438dd7094b9947eb9a5b45dc3d1ead709fde
EasyConcepts <info@easyconcepts.com>
2025-12-17 01:06:00 +0100

    Disable ESLint during build with next-intl plugin

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,14 @@
 /** @type {import('next').NextConfig} */
-const withNextIntl = require('next-intl/plugin')(
-  // This is the default (also the `src` folder is supported out of the box)
-  './src/i18n/request.ts'
-);
+const withNextIntl = require('next-intl/plugin')( './src/i18n/request.ts');
 
 const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
-eslint: {
+  eslint: {
     ignoreDuringBuilds: true,
   },
-
-  // Add any other Next.js config options here
+  
 };
 
 module.exports = withNextIntl(nextConfig);
